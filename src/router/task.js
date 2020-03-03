15c369ee00c30bb35b87b9abd26e154d6f188a39
github username <github email address>
2020-03-03 16:56:57 -0500

    added relationship between user and tasks

--- src/router/task.js
+++ src/router/task.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = new express.Router()
 const Task = require('../controllers/tasks')
+const auth = require('../middleware/auth')
 
-router.post('/tasks/create', Task.create)
+router.post('/tasks/create', auth, Task.create)
 
 module.exports = router
